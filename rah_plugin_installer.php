0bf480050a07750e28fe8e1273613ded7d0537ce
Jukka Svahn <web@rahinaa.biz>
2012-03-20 04:43:01 +0200

    Try to always timeout gracefully.

--- rah_plugin_installer.php
+++ rah_plugin_installer.php
@@ -475,6 +475,9 @@
 
 	function rah_plugin_installer_fget($url, $timeout=10) {
 		
+		@set_time_limit(0);
+		@ignore_user_abort(true);
+		
 		/*
 			If cURL isn't available,
 			use file_get_contents if possible
@@ -503,6 +506,11 @@
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_FAILONERROR, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+
 		$file = curl_exec($ch);
 		$http = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		curl_close($ch);
